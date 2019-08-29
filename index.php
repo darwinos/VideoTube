3f0784ad78c2df86f48c3a0be49f7afdbfba92fc
DarwinOS <darwinos@opensharing.fr>
2019-08-29 03:02:08 +0100

    Added SignIn SignUp Forms

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php require_once("includes/header.php"); ?>
 
-    sideNavContainer content
+<?php
+
+if(isset($_SESSION["userLoggedIn"])) {
+    echo "User is logged in as " . $_SESSION["userLoggedIn"];
+}
+else {
+    echo "Not logged in";
+}
+
+?>
 
 <?php require_once("includes/footer.php"); ?>
